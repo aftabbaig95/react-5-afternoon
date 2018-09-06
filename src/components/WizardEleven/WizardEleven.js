75233a189bc51e0ec7d6683fc3a29f1723c59133
Aftab Baig <aftabbaig@Aftabs-MacBook-Air.local>
2018-09-06 14:38:22 -0500

    commit

--- src/components/WizardEleven/WizardEleven.js
+++ src/components/WizardEleven/WizardEleven.js
@@ -1,6 +1,8 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import {connect} from 'react-redux'; 
+
 class WizardEleven extends Component {
 
     render(){
